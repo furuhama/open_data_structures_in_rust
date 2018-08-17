b816f28bb756218dc49b45f4fb0198555297afee
furuhama <furuhama@users.noreply.github.com>
2018-08-18 03:21:42 +0900

    Read seed_data file as LinkContainer & Print last Node

--- src/modules/read_file.rs
+++ src/modules/read_file.rs
@@ -1,10 +1,15 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use super::list_container::{Node, ListContainer};
 
 pub fn read_file_and_write_from_bottom() {
     let f = File::open("seed_data.txt").expect("No such file or directory");
     let reader = BufReader::new(f);
     let lines = reader.lines();
 
-    println!("{}", lines.last().unwrap().unwrap());
+    let mut list_container = ListContainer::new();
+    lines.for_each(|line| list_container.push_last(Box::new(Node::new(line.unwrap()))));
+
+    let last_node = list_container.pop_last();
+    println!("{:?}", last_node);
 }
