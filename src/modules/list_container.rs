61dd6cbba0f0a44d97befe3ca4c6a48bf7d80820
furuhama <furuhama@users.noreply.github.com>
2018-08-18 03:00:38 +0900

    Add comments

--- src/modules/list_container.rs
+++ src/modules/list_container.rs
@@ -33,16 +33,25 @@ impl ListContainer {
 
     pub fn push_first(&mut self, mut node: Box<Node>) {
         match self.first {
+            // case: ListContainer is empty
             None => {
                 let node_ptr = Box::into_raw_non_null(node);
+
+                // set pointer for container
                 self.first = Some(node_ptr);
                 self.last= Some(node_ptr);
             },
+            // case: ListContainer has some Nodes
             Some(_) => {
                 unsafe {
+                    // set pointer for pushed node
                     node.next = self.first;
+
                     let node_ptr = Box::into_raw_non_null(node);
+
+                    // set pointer for previous first node
                     self.first.unwrap().as_mut().prev = Some(node_ptr);
+                    // set pointer for container
                     self.first = Some(node_ptr);
                 }
             },
@@ -51,16 +60,25 @@ impl ListContainer {
 
     pub fn push_last(&mut self, mut node: Box<Node>) {
         match self.last {
+            // case: ListContainer is empty
             None => {
                 let node_ptr = Box::into_raw_non_null(node);
+
+                // set pointer for container
                 self.first = Some(node_ptr);
                 self.last= Some(node_ptr);
             },
+            // case: ListContainer has some Nodes
             Some(_) => {
                 unsafe {
+                    // set pointer for pushed node
                     node.prev = self.last;
+
                     let node_ptr = Box::into_raw_non_null(node);
+
+                    // set pointer for previous last node
                     self.last.unwrap().as_mut().next = Some(node_ptr);
+                    // set pointer for container
                     self.last = Some(node_ptr);
                 }
             },
@@ -69,20 +87,27 @@ impl ListContainer {
 
     pub fn pop_first(&mut self) -> Option<Box<Node>> {
         match self.first {
+            // case: ListContainer is empty
             None => {
                 None
             },
+            // case: ListContainer has some Nodes
             Some(mut node_ptr) => {
                 unsafe {
                     match node_ptr.as_ref().next {
+                        // case: ListContainer has just one Node
                         None => {
+                            // set pointer for container
                             self.first = None;
                             self.last = None;
 
                             Some(Box::from_raw(node_ptr.as_mut()))
                         },
+                        // case: ListContainer has two or more Nodes
                         Some(next_ptr) => {
+                            // set pointer for container
                             self.first = Some(next_ptr);
+                            // set container for new first node
                             self.first.unwrap().as_mut().prev = None;
 
                             Some(Box::from_raw(node_ptr.as_mut()))
@@ -95,20 +120,27 @@ impl ListContainer {
 
     pub fn pop_last(&mut self) -> Option<Box<Node>> {
         match self.last {
+            // case: ListContainer is empty
             None => {
                 None
             },
+            // case: ListContainer has some Nodes
             Some(mut node_ptr) => {
                 unsafe {
                     match node_ptr.as_ref().prev {
+                        // case: ListContainer has just one Node
                         None => {
+                            // set pointer for container
                             self.first = None;
                             self.last = None;
 
                             Some(Box::from_raw(node_ptr.as_mut()))
                         },
+                        // case: ListContainer has two or more Nodes
                         Some(prev_ptr) => {
+                            // set pointer for container
                             self.last = Some(prev_ptr);
+                            // set pointer for new last node
                             self.last.unwrap().as_mut().next = None;
 
                             Some(Box::from_raw(node_ptr.as_mut()))
