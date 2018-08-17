063d15cc6c58968aa51fba8ea8a6e7e9739787e9
furuhama <furuhama@users.noreply.github.com>
2018-08-18 03:50:22 +0900

    Update process to print very long file

--- src/modules/read_file.rs
+++ src/modules/read_file.rs
@@ -4,15 +4,29 @@ use super::list_container::{Node, ListContainer};
 
 pub fn read_file_and_write_from_bottom() {
     // `seed_data.txt` contains 1,000,000 lines of texts.
-    // let f = File::open("seed_data.txt").expect("No such file or directory");
-    let f = File::open("small_seed_data.txt").expect("No such file or directory");
+    let f = File::open("seed_data.txt").expect("No such file or directory");
+    // let f = File::open("small_seed_data.txt").expect("No such file or directory");
     let reader = BufReader::new(f);
     let lines = reader.lines();
 
     let mut list_container = ListContainer::new();
     lines.for_each(|line| list_container.push_last(Box::new(Node::new(line.unwrap()))));
 
-    print_list_container_from_last(&mut list_container)
+    // print_list_container_from_last(&mut list_container)
+
+    // case: print seed_data.txt
+    loop {
+        let result = list_container.pop_last();
+        match result {
+            None => {
+                println!("=== End ===");
+                break;
+            },
+            Some(node) => {
+                println!("{}", node.ref_content())
+            },
+        }
+    }
 }
 
 // this function uses pattern match and call itself recursively in `Some` pattern.
