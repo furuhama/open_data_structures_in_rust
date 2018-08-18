ec37379f8912f1e9aca5afe5a2e622f747eab592
furuhama <furuhama@users.noreply.github.com>
2018-08-18 14:34:28 +0900

    Implement UniqueSet named USet & Element (test them and all case passed)

--- src/modules/unique_set_container.rs
+++ src/modules/unique_set_container.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct Element {
     content: String,
 }
@@ -9,27 +10,77 @@ impl Element {
         }
     }
 
-    pub fn ref_content(&self) -> &str {
+    fn ref_content(&self) -> &str {
         &self.content
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct USet {
     elements: Vec<Box<Element>>,
+    size: usize,
 }
 
 trait IsUSet {
-    fn size() -> usize;
+    fn size(&self) -> usize;
 
-    fn add(Box<Element>) -> bool;
+    fn add(&mut self, Box<Element>) -> bool;
 
-    fn remove(String) -> Option<Box<Element>>;
+    fn remove(&mut self, String) -> Option<Box<Element>>;
 }
 
 impl USet {
     pub fn new() -> USet {
         USet {
-            elements: Vec::<Box<Element>>::new()
+            elements: Vec::<Box<Element>>::new(),
+            size: 0,
         }
+    }
+}
+
+impl IsUSet for USet {
+    fn size(&self) -> usize {
+        self.size
+    }
+
+    fn add(&mut self, element: Box<Element>) -> bool {
+        match self.elements.iter().find(|&e| *e == element) {
+            None => {
+                self.elements.push(element);
+                self.size += 1;
+                true
+            },
+            Some(_) => {
+                false
+            }
+        }
+    }
+
+    fn remove(&mut self, content: String) -> Option<Box<Element>> {
+        let index = self.elements.iter().position(|e| *e.content == content)?;
+        self.size -= 1;
+        Some(self.elements.remove(index))
+    }
+}
+
+mod test {
+    use super::{Element, USet, IsUSet};
+
+    #[test]
+    fn basic() {
+        let mut uset = USet::new();
+
+        assert!(uset.add(Box::new(Element::new(String::from("one")))));
+        assert_eq!(uset.size, 1);
+        assert!(!uset.add(Box::new(Element::new(String::from("one")))));
+        assert_eq!(uset.size, 1);
+
+        assert!(uset.add(Box::new(Element::new(String::from("two")))));
+        assert_eq!(uset.size, 2);
+
+        assert_eq!(uset.remove(String::from("hogehogefugafuga")), None);
+        assert_eq!(uset.size, 2);
+        assert_eq!(uset.remove(String::from("two")), Some(Box::new(Element::new(String::from("two")))));
+        assert_eq!(uset.size, 1);
     }
 }
