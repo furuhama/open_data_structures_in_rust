fd72d7694074fd9331d4def5a8975e84c830a92f
furuhama <furuhama@users.noreply.github.com>
2018-08-18 12:16:41 +0900

    Edit generate_seed.rb

--- src/ruby/generate_seed.rb
+++ src/ruby/generate_seed.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
 
-File.open('seed_data.txt', 'w') do |f|
-  1_000_000.times do |i|
-    f.puts("test #{i}")
+# File.open('seed_data.txt', 'w') do |f|
+#   1_000_000.times do |i|
+#     f.puts("test #{i}")
+#   end
+# end
+
+RANDOM_WORDS = %w[hoge fuga piyo nyaa myon fuee waay damn].freeze
+
+File.open('small_seed_data.txt', 'w') do |f|
+  237.times do |i|
+    f.puts("#{RANDOM_WORDS.sample} #{i}")
   end
 end
